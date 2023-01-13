8582e414f52fe9b708c0920299a91948a0a568d0
dhq_boiler <dhq_boiler@live.jp>
2023-01-13 15:16:38 +0900

    first commit

---
+++ tools/DiffByWinMerge.bat
@@ -0,0 +1,28 @@
+
+REM %1 : リポジトリのパス
+REM %2 : 指定したsha
+REM %3 : ファイル
+
+chcp 65001
+
+git checkout %2 %3
+
+set p1=%1\%3
+
+more %p1:/=\% > %1\after.txt
+
+setlocal
+for /f "usebackq delims=" %%A in (` git rev-list -1 %2^^ -- %3 `) do set HOGE=%%A
+
+git checkout %HOGE%^^ %3
+
+more %p1:/=\% > %1\before.txt
+
+"C:\Program Files\WinMerge\WinMergeU.exe" %1\before.txt %1\after.txt
+
+git checkout HEAD %3
+
+del %1\before.txt
+del %1\after.txt
+
+exit
